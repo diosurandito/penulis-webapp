e785650736ca63b3b5540b40d2afe092b5a107b2
diosurandito <diosurandito@gmail.com>
2017-10-26 13:11:53 +0700

    init commit

---
+++ src/main/java/lab/kecebong/penuliswebapp/repo/PenulisRepo.java
@@ -0,0 +1,14 @@
+package lab.kecebong.penuliswebapp.repo;
+
+import lab.kecebong.penuliswebapp.entity.Penulis;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface PenulisRepo
+
+	extends JpaRepository<Penulis,String> {
+
+    public Penulis findById(String id);
+}
